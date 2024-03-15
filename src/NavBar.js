82ad9aff1c553bf3da2b74272196442977ee385d
tweee64 <phamthuthuy498@gmail.com>
2024-03-15 17:51:37 +0800

    feat: change navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,8 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import { navLinks } from "./constants";
 import { resume, close, menu } from "./assets";
+import { FiAlignRight, FiX } from "react-icons/fi";
+
 export default function NavBar(props) {
   const { onDarkMode } = props;
   const [active, setActive] = useState("");
@@ -14,12 +16,13 @@ export default function NavBar(props) {
           <nav className="pt-5 mb-12 flex justify-between ">
             <h1 className="text-xl dark:text-neutral-100 ">developedbythuy</h1>
             <div className="flex dark:text-neutral-100">
-              <BsFillMoonStarsFill
-                onClick={onDarkMode}
-                className="cursor-pointer mr-2"
-              />
-
               <ul className="flex-row gap-3 list-none hidden sm:flex  mt-2">
+                <div className="grid content-center ">
+                  <BsFillMoonStarsFill
+                    onClick={onDarkMode}
+                    className="justify-center cursor-pointer mr-2"
+                  />
+                </div>
                 {navLinks.map((nav) => (
                   <li
                     key={nav.id}
@@ -43,53 +46,64 @@ export default function NavBar(props) {
                 </li>{" "}
               </ul>
               {/* mobile */}
-              <div className="sm:hidden flex flex-1 w-screen justify-end items-center">
+              <div className=" sm:hidden flex flex-1 w-max justify-end items-center">
                 {toggle ? (
-                  <div
-                    className={`p-6 bg-flashWhite opacity-[0.98] absolute 
-                top-0 left-0 w-screen h-[100vh] z-10 menu ${
-                  toggle ? "menu-open" : "menu-close"
-                }`}
-                  >
+                  <div className="">
                     <div className="flex justify-end">
-                      <img
-                        src={close}
-                        alt="close"
+                      <div className="grid content-center ">
+                        <BsFillMoonStarsFill
+                          onClick={onDarkMode}
+                          className="justify-center cursor-pointer mr-2"
+                        />
+                      </div>
+                      <FiX
                         className="w-[22px] h-[22px] object-contain cursor-pointer"
                         onClick={() => setToggle(!toggle)}
                       />
                     </div>
                     <ul
                       className="list-none flex flex-col -gap-[1rem] 
-                items-start justify-end mt-[10rem] -ml-[35px]"
+                items-start justify-end  -ml-[35px] "
                     >
                       {navLinks.map((nav) => (
                         <li
-                          id={nav.id}
                           key={nav.id}
                           className={`${
                             active === nav.title
                               ? "text-french"
                               : "text-eerieBlack"
-                          } text-[88px] font-bold font-arenq 
-                      uppercase tracking-[1px] cursor-pointer`}
-                          onClick={() => {
-                            setToggle(!toggle);
-                            setActive(nav.title);
-                          }}
+                          } hover:text-taupe 
+                           cursor-pointer nav-links mt-2 dark:text-neutral-100`}
+                          onClick={() => setActive(nav.title)}
                         >
                           <a href={`#${nav.id}`}>{nav.title}</a>
                         </li>
                       ))}
+                      <li className="mt-3">
+                        <a
+                          href={resume}
+                          download
+                          className="bg-color-button dark:bg-teal-500 text-white  px-4 py-2 rounded-md "
+                        >
+                          Resume
+                        </a>
+                      </li>{" "}
                     </ul>
                   </div>
                 ) : (
-                  <img
-                    src={menu}
-                    alt="menu"
-                    className="w-[34px] h-[34px] object-contain cursor-pointer"
-                    onClick={() => setToggle(!toggle)}
-                  />
+                  <div className="flex justify-end">
+                    <div className="grid content-center ">
+                      <BsFillMoonStarsFill
+                        onClick={onDarkMode}
+                        className="justify-center cursor-pointer mr-2"
+                      />
+                    </div>
+
+                    <FiAlignRight
+                      className="w-[22px] h-[22px] object-contain cursor-pointer"
+                      onClick={() => setToggle(!toggle)}
+                    />
+                  </div>
                 )}
               </div>
             </div>
